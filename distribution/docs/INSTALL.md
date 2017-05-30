940029cba0660e897a501f047b21652849be63d6
Joel Haasnoot <joelhaasnoot@gmail.com>
2017-05-30 21:10:39 +0200

    Change namespacing of configuration to make it easier to make one config file per server.
    Configuration can be overriden with runtime property -Darchaius.configurationSource.additionalUrls=file://<path>

---
+++ distribution/docs/INSTALL.md
@@ -0,0 +1,7 @@
+Installatiehandboek
+==========
+
+Configuratie
+------------
+De default configuratie maakt deel uit van de applicatie en is te vinden in ```src/resources/configuration.properties```. 
+Dit kan runtime worden aangepast door de volgende property te specificeren: ```-Darchaius.configurationSource.additionalUrls=file://<path>```, waarbij path
--- distribution/src/main/java/nl/crowndov/displaydirect/distribution/Configuration.java
+++ distribution/src/main/java/nl/crowndov/displaydirect/distribution/Configuration.java
@@ -21,15 +21,15 @@ public class Configuration {
     private static AbstractConfiguration config = ConfigurationManager.getConfigInstance();
 
     public static List<String> getKvEndpoints() {
-        return Arrays.asList(config.getStringArray("kv78turbo.endpoints"));
+        return Arrays.asList(config.getStringArray("distribution.kv78turbo.endpoints"));
     }
 
     public static String[] getKvPublishers() {
-        return config.getStringArray("kv78turbo.publishers");
+        return config.getStringArray("distribution.kv78turbo.publishers");
     }
 
     public static ZoneId getZoneId() {
-        return ZoneId.of(config.getString("timezone.default"));
+        return ZoneId.of(config.getString("distribution.timezone.default"));
     }
 
     public static String getOutputTransport() {
@@ -37,7 +37,7 @@ public class Configuration {
     }
 
     public static String getMqttBrokerHost() {
-        return config.getString("mqtt.host", "localhost:1883");
+        return config.getString("distribution.mqtt.host", "localhost:1883");
     }
 
     public static int getZmqInternalPort() {
@@ -45,67 +45,67 @@ public class Configuration {
     }
 
     public static String getMqttClientId() {
-        return config.getString("mqtt.client_id", "distribution");
+        return config.getString("distribution.mqtt.client_id", "distribution");
     }
 
     public static int getAuthorizationTokenMaxAge() {
-        return config.getInt("authorization.max_age_hours");
+        return config.getInt("distribution.authorization.max_age_hours");
     }
 
     public static String getBaseUrl() {
-        return config.getString("install.base_url", "http://localhost:8080");
+        return config.getString("distribution.install.base_url", "http://localhost:8080");
     }
 
     public static String getSmtpUsername() {
-        return config.getString("smtp.username", null);
+        return config.getString("distribution.smtp.username", null);
     }
 
     public static String getSmtpPassword() {
-        return config.getString("smtp.password", null);
+        return config.getString("distribution.smtp.password", null);
     }
 
     public static String getSmtpFrom() {
-        return config.getString("smtp.from");
+        return config.getString("distribution.smtp.from");
     }
 
     public static String getSmtpHostname() {
-        return config.getString("smtp.hostname");
+        return config.getString("distribution.smtp.hostname");
     }
 
     public static String getSmtpReplyTo() {
-        return config.getString("smtp.reply_to");
+        return config.getString("distribution.smtp.reply_to");
     }
 
     public static int getSmtpPort() {
-        return config.getInt("smtp.port", 25);
+        return config.getInt("distribution.smtp.port", 25);
     }
 
     public static int getSmtpConnectTimeout() {
-        return  config.getInt("smtp.connect_timeout", 10*1000);
+        return  config.getInt("distribution.smtp.connect_timeout", 10*1000);
     }
 
     public static String getFileBasePath() {
-        return config.getString("backup.path", System.getProperty("java.io.tmpdir"));
+        return config.getString("distribution.backup.path", System.getProperty("java.io.tmpdir"));
     }
 
     public static String getKv7PlanningPath() {
-        return config.getString("kv78turbo.path.planning");
+        return config.getString("distribution.kv78turbo.path.planning");
     }
 
     public static String getKv7CalendarPath() {
-        return config.getString("kv78turbo.path.calendar");
+        return config.getString("distribution.kv78turbo.path.calendar");
     }
 
     public static String getChbPath() {
-        return config.getString("chb.path");
+        return config.getString("distribution.chb.path");
     }
 
 
     public static String getPlanningLoaderTime() {
-        return config.getString("planning.loadTime", "03:30");
+        return config.getString("distribution.planning.loadTime", "03:30");
     }
 
     public static String getPlanningCleanupTime() {
-        return config.getString("planning.loadTime", "06:00");
+        return config.getString("distribution.planning.cleanupTime", "04:00");
     }
 }
